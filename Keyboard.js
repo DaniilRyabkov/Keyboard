e6f03d0b5191ff0f3a47aad12e1aec154956e554
Daniil <skylaser33@gmail.com>
2024-03-02 18:55:30 +0300

    the first part of the program Control the ball from the keyboard

--- Keyboard.js
+++ Keyboard.js
@@ -1,3 +1,17 @@
+var keyboard = {
+87:"W",
+83:"S",
+65:"A",
+68:"D",
+32:"пробел",
+16:"shift",
+13:"inter",
+37:"Лево",
+39:"Право",
+38:"Вверх",
+40:"Вниз",
+
+};
 $ ("body").keydown(function (event) {
-    console.log(event.keyCode);
+    console.log (keyboard[event.keyCode]);
 });
